5fd33a9e5ea8f47e1de496d52f8c180ac2ddf49f
TRsky <625310581@qq.com>
2018-02-14 20:14:45 +0800

    a django blog

---
+++ blogproject/blog/urls.py
@@ -0,0 +1,9 @@
+# blog/urls.py
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
---
+++ blogproject/blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from .models import Post
+
+"""
+请使用下方的模板引擎方式。
+def index(request):
+    return HttpResponse("欢迎访问我的博客首页！")
+"""
+
+"""
+请使用下方真正的首页视图函数
+def index(request):
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
+"""
+
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
