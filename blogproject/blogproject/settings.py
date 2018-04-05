98b9af003876d04186b355cf0c4adc48b15d2358
TRsky <625310581@qq.com>
2018-04-05 18:50:23 +0800

    move to remote

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+lp8+c1b+s$xsz&ebw-%s+lrz1x0ha301t$&&zs^j63n0=v0ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.callmebomb.club']
 
 
 # Application definition
@@ -125,3 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 加入下面的配置
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
