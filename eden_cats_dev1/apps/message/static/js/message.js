242f71f4f7b8a769b72d345a2460e1c7021e4d77
Trsky <625310581@qq.com>
2018-09-16 22:15:57 +0800

    style: beautify the message_board css style

--- eden_cats_dev1/apps/message/static/js/message.js
+++ eden_cats_dev1/apps/message/static/js/message.js
@@ -2,16 +2,25 @@
   留言框字数限制
  +-------------------------- */
 
-   function checkLength(which) {
-	var maxChars = 280; //
-	if(which.value.length > maxChars){
-		alert("您出入的字数超出 280 字节的限制，自动截取 280 字节！");
-		// 超过限制的字数了就将 文本框中的内容按规定的字数 截取
-		which.value = which.value.substring(0,maxChars);
-		return false;
-	}else{
-		var curr = maxChars - which.value.length; //250 减去 当前输入的
-		document.getElementById("conBox").innerHTML = curr.toString();
-		return true;
-	}
+function checkLength(which) {
+    var maxChars = 280; //
+    if (which.value.length > maxChars) {
+        alert("您出入的字数超出 280 字节的限制，自动截取 280 字节！");
+        // 超过限制的字数了就将 文本框中的内容按规定的字数 截取
+        which.value = which.value.substring(0, maxChars);
+        return false;
+    } else {
+        var curr = maxChars - which.value.length; //250 减去 当前输入的
+        document.getElementById("conBox").innerHTML = curr.toString();
+        return true;
+    }
+}
+
+
+function rec() {
+    if (confirm("确认删除该条留言?")) {
+        a.setAttribute("onclick", "");
+        return true;
+    }
+    return false;
 }
