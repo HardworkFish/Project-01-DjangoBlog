1e5dd368740a18b732dbf7d4c39fe110d6f49dcb
Trsky <625310581@qq.com>
2018-09-18 23:29:39 +0800

    feat: add like the article function

--- eden_cats_dev1/apps/easy_comment/models.py
+++ eden_cats_dev1/apps/easy_comment/models.py
@@ -2,10 +2,32 @@ from django.db import models
 from django.conf import settings
 from mptt.models import TreeForeignKey, MPTTModel
 from ckeditor_uploader.fields import RichTextUploadingField
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 
+class Favour(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='favours', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    date_created = models.DateTimeField(auto_now_add=True)
+    liked = models.BooleanField(default=True, verbose_name='是否喜欢')
 
+    class Meta:
+        ordering = ('-date_created',)
+        unique_together = ('user', 'content_type', 'object_id')
+
+    def __str__(self):
+        if self.liked:
+            return "{} 喜欢 {} 链接：/article/{}/".format(self.user.username, self.content_object.title, self.object_id)
+        else:
+            return "{} 取消了喜欢 {}_{}".format(self.user.username, self.content_object.title,
+                                        self.object_id)
+
+
+# 评论
 class Comment(MPTTModel):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=50, blank=True, null=True)
@@ -23,6 +45,7 @@ class Comment(MPTTModel):
         return '%s 评论文章 post_%s' % (self.user_name, str(self.post.id))
 
 
+# 为评论点赞
 class Like(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
